fbe9c5f1e1abd8c534ae423f5e7a71d8af8f1cb8
unknown <iloverahulkumar@gmail.com>
2023-05-05 20:49:29 +0530

    day-3-sohail

--- script/hotel.js
+++ script/hotel.js
@@ -20,7 +20,60 @@ let total_guest = document.getElementById("total-guest")
 let decrease_guest = document.getElementById("decrease-guest")
 let apply_button = document.getElementById("apply-button")
 
+let mainSection=document.getElementById("product_container")
 
 
+// console.log({ "price": priceFrom, "priceTo": priceTo, "property": property_type })
+async function fetchdata(){
+try{
+let res=await fetch("https://mock-api-hotels.onrender.com/hotels")
+let data=await res.json()
+console.log(data)
+display(data);
 
-console.log({ "price": priceFrom, "priceTo": priceTo, "property": property_type })
+}
+catch(err){
+    console.log(err);
+}
+}
+fetchdata();
+
+function display(data){
+mainSection.innerHTML="";
+
+let cardlist=document.createElement("div")
+cardlist.className="card-list";
+mainSection.append(cardlist)
+data.forEach((el)=>{
+    let card=createcard(el);
+    cardlist.append(card);
+})
+}
+
+function createcard(el){
+    let card=document.createElement("div");
+    card.className="card";
+    let img=document.createElement("img")
+    img.src=el.image;
+    img.className="hotelImg";
+    let carditem=document.createElement("div")
+    carditem.className="card-item";
+    let h3=document.createElement("h3")
+    h3.textContent=el.name;
+    let location=document.createElement("p")
+    location.textContent=el.location
+    let h1=document.createElement("h1")
+    h1.textContent=`$${el.price}`
+    let detail=document.createElement("button")
+    detail.className="detail"
+    detail.textContent="View Details"
+    let book=document.createElement("button")
+    book.className="book"
+    book.textContent="Book Now"
+    
+    carditem.append(h3,location,h1,detail,book)
+    card.append(img,carditem)
+    return card;
+
+
+}
