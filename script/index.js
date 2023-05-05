3f52faa2a8bb4024e5cf53b581ebeba2f671bcb2
shivendra-somr <shivendra.somr@gmail.com>
2023-05-06 01:17:31 +0530

    login as admin/user

--- script/index.js
+++ script/index.js
@@ -1,3 +1,43 @@
+let aboutus = document.querySelector("#about")
+let perks = document.querySelector("#container1")
+let FAQs = document.querySelector("#faqs")
+
+let aboutusSection = document.querySelector(".heading")
+let perksSection = document.querySelector('#container1')
+let faqSection = document.querySelector('.faq')
+
+function scrollToSection(section) {
+    section.scrollIntoView({ behavior: "smooth" });
+}
+
+// Attach event listeners to the buttons
+aboutus.addEventListener('click', function () {
+    scrollToSection(aboutusSection);
+});
+
+perks.addEventListener('click', function () {
+    scrollToSection(perksSection);
+});
+FAQs.addEventListener('click', function () {
+    scrollToSection(faqSection);
+});
+
+
+// join member click event
+
+let join_Member = document.getElementById("join");
+let joinSection = document.getElementById('joinSection');
+
+join_Member.addEventListener("click", () => {
+    scrollToSection(joinSection);
+})
+
+let logo = document.getElementById("logo");
+
+logo.addEventListener("click", () => {
+    window.location.href = "./index.html"
+})
+
 //   FAQs Section
 
 var acc = document.getElementsByClassName("accordion");
@@ -17,15 +57,11 @@ for (i = 0; i < acc.length; i++) {
     });
 }
 
+// Join Section
 
-// join member click event
+const join = document.getElementById('joinM');
 
-let join_Member = document.getElementById("join");
-
-join_Member.addEventListener("click", () => {
-    window.location.href = "./hotel.html"
-})
-let logo = document.getElementById("logo")
-logo.addEventListener("click", () => {
-    window.location.href = "./index.html"
+join.addEventListener('click', ()=>{
+    console.log('join')
+    window.location.href = "./login.html"
 })
---
+++ script/login.js
@@ -0,0 +1,48 @@
+
+const logoHome = document.getElementById('logo');
+
+logoHome.addEventListener('click', () => {
+  window.location.href = './index.html';
+})
+
+
+const loginText = document.querySelector(".title-text .login");
+const loginForm = document.querySelector("form.login");
+const loginBtn = document.querySelector("label.login");
+const signupBtn = document.querySelector("label.signup");
+const signupLink = document.querySelector("form .signup-link a");
+
+// signup
+signupBtn.onclick = (() => {
+  loginForm.style.marginLeft = "-50%";
+  loginText.style.marginLeft = "-50%";
+});
+
+//login
+loginBtn.onclick = (() => {
+  loginForm.style.marginLeft = "0%";
+  loginText.style.marginLeft = "0%";
+});
+
+// signup click
+signupLink.onclick = (() => {
+  signupBtn.click();
+  return false;
+});
+
+const adminData = { "username": "admin", "password" : "admin123"};
+
+const loginFormBtn = document.querySelector("input[type='submit']");
+const loginUsername = loginForm.querySelector("input[type='text']");
+const loginPassword = loginForm.querySelector("input[type='password']");
+
+loginFormBtn.addEventListener('click', (e)=>{
+  e.preventDefault();
+  // console.log(adminData,loginUsername.value,loginPassword.value);
+  if (loginUsername.value === adminData.username && loginPassword.value === adminData.password){
+    window.location.href = "./hotelfiltered.html";
+  }
+  else{
+    window.location.href = "./hotel.html";
+  }
+})
