711b6cd6c5bf30b5e668bf82953529aeb22af8d2
unknown <iloverahulkumar@gmail.com>
2023-05-06 15:38:54 +0530

    day-3

--- script/hotel.js
+++ script/hotel.js
@@ -5,10 +5,7 @@ logo.addEventListener("click", () => {
 })
 
 // reset Input of filter
-function resetInputs() {
-    let inputs = document.getElementsByClassName("reset-input")
 
-}
 
 
 // filtering
@@ -20,164 +17,176 @@ let increase_guest = document.getElementById("increase-guest")
 let total_guest = document.getElementById("total-guest")
 let decrease_guest = document.getElementById("decrease-guest")
 
+let resett = document.getElementById("resett")
 
-
-let mainSection=document.getElementById("product_container")
-let noofcards=document.getElementById("noofcards")
+let mainSection = document.getElementById("product_container")
+let noofcards = document.getElementById("noofcards")
 fetchdata();
-// console.log({ "price": priceFrom, "priceTo": priceTo, "property": property_type })
-async function fetchdata(){
-    try{
-        let res=await fetch("https://mock-api-hotels.onrender.com/hotels")
-        let data=await res.json();
-        // arr=data;
+
+
+// fetchData function
+async function fetchdata() {
+    try {
+        let res = await fetch("https://mock-api-hotels.onrender.com/hotels")
+        let data = await res.json();
         console.log(data)
-        
         display(data);
-        let search=document.getElementById("search")
-search.addEventListener("input",()=>{
-    searchdata(data);
-})
+        let search = document.getElementById("search")
 
-property_type.addEventListener("change",()=>{
-    propertytype(data);
-})
-let apply_button = document.getElementById("apply-button")
-apply_button.addEventListener("click",()=>{
-   
-    pricefilter(data);
-})
-        
-        }
-        catch(err){
-                console.log(err);
-            }
-            }
-   
-    // function fetchdata() {
-    //     fetch("https://mock-api-hotels.onrender.com/hotels")
-    //       .then(res => res.json())
-    //       .then(data => {
-            
-    //         console.log("Data successfully fetched:", arr);
-    //         display(data);
-    //         console.log(data);
-           
-    //       })
-    //       .catch(error => {
-    //         console.error("Error fetching data:", error);
-    //       });
-    //   }
+        // search addEventListener
+        search.addEventListener("input", () => {
+            searchdata(data);
+        })
+
+// property_type addEventListener
+        property_type.addEventListener("change", () => {
+            propertytype(data);
+        })
+
+        // apply_button addEventListener for filter data by price
+        let apply_button = document.getElementById("apply-button")
+        apply_button.addEventListener("click", () => {
+
+            pricefilter(data);
+        })
+
+        // reset input addEventListener
+        resett.addEventListener("click", () => {
+            resetInputs(data);
+        })
 
 
-function display(data){
-mainSection.innerHTML="";
-noofcards.textContent=`${data.length} Results`;
-let cardlist=document.createElement("div")
-cardlist.className="card-list";
-mainSection.append(cardlist)
-data.forEach((el)=>{
-    let card=createcard(el);
-    cardlist.append(card);
-})
+
+    }
+    catch (err) {
+        console.log(err);
+    }
 }
 
-function createcard(el){
-    let card=document.createElement("div");
-    card.className="card";
-    let img=document.createElement("img")
-    img.src=el.image;
-    img.className="hotelImg";
-    let carditem=document.createElement("div")
-    carditem.className="card-item";
-    let h3=document.createElement("h2")
-    h3.style.marginBottom ="10px";
-    h3.textContent=el.name;
-    let location=document.createElement("p")
-    location.textContent=el.location
-    
-    let h1=document.createElement("h1")
-    h1.textContent=`₹${el.price}`
-    let span=document.createElement("span")
-    span.textContent=" /Night"
-    span.className="span1"
-    let divprice=document.createElement("div")
-    divprice.className="divprice"
-    divprice.append(h1,span)
-    let detail=document.createElement("button")
-    detail.className="detail"
-    detail.textContent="View Details"
-    let book=document.createElement("button")
-    book.className="book"
-    book.textContent="Book Now"
-    
-    carditem.append(h3,location,divprice,detail,book)
-    card.append(img,carditem)
+
+
+// display function
+function display(data) {
+    mainSection.innerHTML = "";
+
+    // function to increaseor decrease noofcards. of guests
+    let count = 1;
+    increase_guest.addEventListener("click", function () {
+        count++;
+        total_guest.textContent = count;})
+
+    decrease_guest.addEventListener("click", function () {
+        if (count > 1) {
+            count--;
+            total_guest.textContent = count;
+        }})
+
+    noofcards.textContent = `${data.length} Results`;
+    let cardlist = document.createElement("div")
+    cardlist.className = "card-list";
+    mainSection.append(cardlist)
+    data.forEach((el) => {
+        let card = createcard(el);
+        cardlist.append(card);
+    })
+}
+
+// createcard function 
+function createcard(el) {
+    let card = document.createElement("div");
+    card.className = "card";
+    let img = document.createElement("img")
+    img.src = el.image;
+    img.className = "hotelImg";
+    let carditem = document.createElement("div")
+    carditem.className = "card-item";
+    let h3 = document.createElement("h2")
+    h3.style.marginBottom = "10px";
+    h3.textContent = el.name;
+    let location = document.createElement("p")
+    location.textContent = el.location
+
+    let h1 = document.createElement("h1")
+    h1.textContent = `₹${el.price}`
+    let span = document.createElement("span")
+    span.textContent = " /Night"
+    span.className = "span1"
+    let divprice = document.createElement("div")
+    divprice.className = "divprice"
+    divprice.append(h1, span)
+    let detail = document.createElement("button")
+    detail.className = "detail"
+    detail.textContent = "View Details"
+    let book = document.createElement("button")
+    book.className = "book"
+    book.textContent = "Book Now"
+
+    carditem.append(h3, location, divprice, detail, book)
+    card.append(img, carditem)
     return card;
 
 
 }
-// console.log(arr)
 
-// search functionaq
-function searchdata(data){
-     let filtered=data.filter(function(el){
-            if(el.name.toUpperCase().includes(search.value.toUpperCase())==true||el.location.toUpperCase().includes(search.value.toUpperCase())==true){
-                return true;
-            }
-            else{
-                return false
-            }
-                })  
-   
-    
+
+// search function
+function searchdata(data) {
+    let filtered = data.filter(function (el) {
+        if (el.name.toUpperCase().includes(search.value.toUpperCase()) == true || el.location.toUpperCase().includes(search.value.toUpperCase()) == true) {
+            return true;
+        }
+        else {
+            return false
+        }
+    })
+
+
 
     display(filtered);
 }
 
-function pricefilter(data){
-    console.log(data)
-    let filt=data.filter((el)=>{
-        // if(priceFrom.value==""){
-        //     return true
-        // }
- if(+(el.price)>priceFrom.value&&+(el.price)<priceTo.value){
-    return true;
-}
-else{
-    return false
-}
-    });
-    display(filt);
-    
-}
-function propertytype(data){
+// pricefilter function 
+function pricefilter(data) {
     // console.log(data)
-    let filt=data.filter((el)=>{
-if(el.type==property_type.value){
-    return true;
+    if(priceFrom.value==""){
+        display(data)
+    }
+    else{
+        let filt = data.filter((el) => {
+            if (+(el.price) > priceFrom.value && +(el.price) < priceTo.value) {
+                return true;
+            }
+            else {
+                return false
+            }
+        });
+        display(filt);
+    }
+   
+
 }
-else{
-    return false
-}
+
+// property_type filter function 
+function propertytype(data) {
+    let filt = data.filter((el) => {
+        if (el.type == property_type.value) {
+            return true;
+        }
+        else {
+            return false
+        }
     });
     display(filt);
-    
+
 }
-let count=1;
-increase_guest.addEventListener("click",function(){
-   count++;
-    total_guest.textContent=count;
-   
-})
-
-decrease_guest.addEventListener("click",function(){
-   if(count>1){
-count--;
-       total_guest.textContent=count;
-   }
-   
-})
 
 
+// reset input function
+function resetInputs(data) {
+   priceFrom.value = ""
+    priceTo.value = ""
+    property_type.value = ""
+    total_guest.textContent = 1;
+    display(data);
 
+}
