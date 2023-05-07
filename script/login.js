50540b56143904f1c436562f3fe72eebc5711a35
gauravraj2601 <gauravraj2601@gmail.com>
2023-05-08 00:35:28 +0530

    login signup authentication

--- script/login.js
+++ script/login.js
@@ -32,19 +32,76 @@ signupLink.onclick = (() => {
 
 const adminData = { "username": "admin", "password" : "admin123"};
 
-const loginFormBtn = document.querySelector("input[type='submit']");
-const loginUsername = loginForm.querySelector("input[type='text']");
+const loginFormBtn = loginForm.querySelector("input[type='submit']");
+const loginUsername = loginForm.querySelector("input[type='email']");
 const loginPassword = loginForm.querySelector("input[type='password']");
 
 loginFormBtn.addEventListener('click', (e)=>{
   e.preventDefault();
   // console.log(adminData,loginUsername.value,loginPassword.value);
-  if (loginUsername.value === adminData.username && loginPassword.value === adminData.password){
+  if(loginUsername.value=="" || loginPassword.value==""){
+        alert("Fill the required Login details")
+  }
+   else if (loginUsername.value === adminData.username && loginPassword.value === adminData.password){
+    alert("Welcome Admin")
     window.location.href = "./hotelAdmin.html";
-    
   }
-  else{
-    window.location.href = "./hotel.html";
+  else {
+    fetch("https://mock-api-hotels.onrender.com/users")
+    .then(res=>res.json())
+    .then((data)=>{
+      let userFound = false;
+      for(let i=0; i<data.length;i++){
+        if(( data[i].email==loginUsername.value  && data[i].password == loginPassword.value)){
+          console.log(data)
+          alert(`Welcome Back ${data[i].name}`)
+          window.location.href = "./hotel.html";
+          userFound = true;
+          break;
+        }
+      }
+      if (!userFound) {
+        alert("Wrong Credentials");
+        loginForm.reset()
+      }
+    })
+    .catch(err=> console.log(err))
   }
+  
 })
 
+let signUpForm = document.querySelector('form.signup');
+let signUpFormBtn = signUpForm.querySelector("input[type='submit']");
+let signUpName = signUpForm.querySelector("input[type='text']");
+let signUpEmail = signUpForm.querySelector("input[type='email']");
+let signUpPassword = document.getElementById("password")
+let signUpConfirmPassword = document.getElementById("confirm-password")
+
+signUpFormBtn.addEventListener("click",(e)=>{
+  e.preventDefault();
+  console.log("hai")
+  if(signUpPassword.value !== signUpConfirmPassword.value){
+    alert("Passwords do not match");
+  }
+  else{
+    let newSignUp = {
+      name: signUpName.value,
+      email: signUpEmail.value,
+      password: signUpPassword.value
+    };
+    fetch("https://mock-api-hotels.onrender.com/users",{
+      method: "POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify(newSignUp)
+    })
+    .then(res => res.json())
+    .then(data => {
+      alert("Signup successful!");
+    })
+    .catch(error => console.error(error));
+  }
+
+  signUpForm.reset();
+});
